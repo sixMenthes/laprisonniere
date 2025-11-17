86bfc3c7915b1a178f2bc9b10ff4b8ccc748677f
sixmenthes <six.menthes@gmail.com>
2025-11-17 18:22:17 +0100

    add by batches feature

--- sentence_embeddings/calculate_embeddings.py
+++ sentence_embeddings/calculate_embeddings.py
@@ -15,24 +15,40 @@ def créer_index_faiss(embeddings):
     d = embeddings.size(1)
     index = faiss.IndexFlatL2(d)
     embeddings_np = embeddings.cpu().numpy().astype('float32')
-    index.add(embeddings)
+    index.add(embeddings_np)
     faiss.write_index(index, "../corpus/whole_proust/corpus_embeddings.index")
 
+def encoder_par_batch(liste_corpus, n):
 
-
-if __name__ == "__main__":
-    print(f"Loading tokenizers etc.")
+    print(f"Loading tokenizer and model...")
     tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
     model = AutoModel.from_pretrained('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
-    d = 384
+    all_embeddings = []
 
+    for i in range(0, len(liste_corpus), n):
+        print(f"itération nº {i}/{len(liste_corpus)//n}")
+        phrases = liste_corpus[i:i + n]
+
+        encoded_input = tokenizer(phrases, padding=True, truncation=True, return_tensors='pt')
+
+        with torch.no_grad():
+            model_output = model(**encoded_input)
+        
+        all_embeddings.append(mean_pooling(model_output, encoded_input['attention_mask']))
+
+    sentence_embeddings = torch.cat(all_embeddings, dim=0)
+
+
+    return sentence_embeddings
+
+    
+
+def main():
     with open('../corpus/whole_proust/corpus.pickle', 'rb') as f:
         phrases = pickle.load(f)
-    print(f"Corpus loaded. Tokenizing corpus.")
-    encoded_input = tokenizer(phrases, padding=True, truncation=True, return_tensors='pt')
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-    print(f"Calculating embeddings.")
-    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    print(f"Creating vector database.")
+
+    sentence_embeddings = encoder_par_batch(phrases, 2000)
     créer_index_faiss(sentence_embeddings)
+
+if __name__ == "__main__":
+    main()
