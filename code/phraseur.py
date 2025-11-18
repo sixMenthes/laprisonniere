fdc72d491a15e2b37d74542d2dc7a9c841084c54
sixmenthes <six.menthes@gmail.com>
2025-11-18 23:33:24 +0100

    ajouté doublons check

--- code/phraseur.py
+++ code/phraseur.py
@@ -1,29 +1,34 @@
 import os
 import re
+from collections import defaultdict
 from tqdm import tqdm
 import pickle
 import spacy
 from spacy.tokens import DocBin
 
+
 nlp = spacy.load("fr_dep_news_trf")
 proust_path = os.path.abspath('../corpus_echos/')
 resultats_path = os.path.abspath('../results/')
+dict_doublons = defaultdict(list)
 
 def préprocesser_corpus(texte:str):
     texte = re.sub(r'\n', ' ', texte)
     texte = re.sub(r' +', ' ', texte)
     return texte
 
-def phraser_doc(spacy_doc):
+def phraser_doc(spacy_doc, titre_oeuvre):
     phrases_tokénisées = []
     phrases_spacy = DocBin()
     for p in spacy_doc.sents:
         phrase = p.text.strip()
-        if len(phrase) > 1 and any(c.isalnum() for c in phrase):
+        dict_doublons[phrase].append(titre_oeuvre)
+        if len(phrase) > 1 and any(c.isalnum() for c in phrase) \
+            and len(dict_doublons[phrase]) == 1:
             phrases_spacy.add(p.as_doc())
             phrases_tokénisées.append(phrase)
+
     return phrases_spacy, phrases_tokénisées
-        
 
 def phraser_corpus(corpus_path):
     recherche_parsée = DocBin()
@@ -36,18 +41,24 @@ def phraser_corpus(corpus_path):
         with open(oeuvre_path, "r") as t:
             texte = préprocesser_corpus(t.read())
             doc = nlp(texte)
-            s, p = phraser_doc(doc)
+            s, p = phraser_doc(doc, oeuvre)
             recherche_parsée.merge(s)
             recherche_texte.extend(p)
     return recherche_parsée, recherche_texte
 
 def main():
     doc_path = os.path.join(resultats_path, 'recherche_parsée.spacy')
-    cornichon_path = os.path.join(resultats_path, 'corpus.pickle')
+    cornichon_phrases = os.path.join(resultats_path, 'corpus_en_phrases.pickle')
+    cornichon_doublons = os.path.join(resultats_path, 'doublons.pickle')
+
     doc_bin, phrases = phraser_corpus(proust_path)
     doc_bin.to_disk(doc_path)
-    with open(cornichon_path, 'wb') as f:
+
+    with open(cornichon_phrases, 'wb') as f:
         pickle.dump(phrases, f)
+    
+    with open(cornichon_doublons, "wb") as f:
+        pickle.dump(dict_doublons, f)
     
 
 
