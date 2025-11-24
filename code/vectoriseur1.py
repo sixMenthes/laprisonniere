b2deb5472491a74470ccf291806091552d7f5d0c
sixmenthes <six.menthes@gmail.com>
2025-11-24 13:08:34 +0100

    add first version of jina model on vectoriseur

--- code/vectoriseur1.py
+++ code/vectoriseur1.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import torch
 import torch.nn.functional as F
@@ -11,6 +10,12 @@ from transformers import AutoTokenizer, AutoModel
 import pickle
 from tqdm import tqdm
 
+resultats_chemin = os.path.abspath('./results/')
+tokenizer = AutoTokenizer.from_pretrained("jinaai/jina-embeddings-v3")
+model = AutoModel.from_pretrained("jinaai/jina-embeddings-v3", trust_remote_code=True)
+task = 'text-matching'
+task_id = model._adaptation_map[task]
+
 
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0]
@@ -21,21 +26,37 @@ def mean_pooling(model_output, attention_mask):
         input_mask_expanded.sum(1), min=1e-9
     )
 
+def encoder_par_batch(liste_corpus, n):
+
+    tous_embeddings = []
+    for i in tqdm(range(0, len(liste_corpus), n), desc="Encodage par batch"):
+        phrases = liste_corpus[i:i + n]
+        encoded_input = tokenizer(phrases, padding=True, truncation=True, return_tensors='pt')
+        adapter_mask = torch.full((len(phrases),), task_id, dtype=torch.int32)
+        with torch.no_grad():
+            model_output = model(**encoded_input, adapter_mask=adapter_mask)
+        embeddings = mean_pooling(model_output, encoded_input["attention_mask"])
+        embeddings = F.normalize(embeddings, p=2, dim=1)
+        tous_embeddings.append(embeddings)
+    tous_embeddings = torch.cat(tous_embeddings, dim=0)
+    return tous_embeddings
+
+def créer_index_faiss(embeddings, index_path):
+    d = embeddings.size(1)
+    index = faiss.IndexFlatL2(d) 
+    embeddings_np = embeddings.cpu().numpy().astype('float32')
+    index.add(embeddings_np)
+    faiss.write_index(index, index_path)
+
+
 def main():
-    sentences = ["How is the weather today?", "What is the current weather like today?"]
+    cornichon = os.path.join(resultats_chemin, 'corpus_en_phrases.pickle')
+    indice_chemin = os.path.join(resultats_chemin, 'vecteurs.index')
+    with open(cornichon, 'rb') as f:
+        phrases = pickle.load(f)
+    sentence_embeddings = encoder_par_batch(phrases, 64)
+    créer_index_faiss(sentence_embeddings, indice_chemin)
 
-    tokenizer = AutoTokenizer.from_pretrained("jinaai/jina-embeddings-v3")
-    model = AutoModel.from_pretrained("jinaai/jina-embeddings-v3", trust_remote_code=True)
-
-    encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
-    task = 'retrieval.query'
-    task_id = model._adaptation_map[task]
-    adapter_mask = torch.full((len(sentences),), task_id, dtype=torch.int32)
-    with torch.no_grad():
-        model_output = model(**encoded_input, adapter_mask=adapter_mask)
-
-    embeddings = mean_pooling(model_output, encoded_input["attention_mask"])
-    embeddings = F.normalize(embeddings, p=2, dim=1)
 
 if __name__ == "__main__":
     main()
