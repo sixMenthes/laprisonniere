ae02b9e496ba2e22cea7c33f635ba95fe88de60d
sixmenthes <six.menthes@gmail.com>
2025-11-28 18:38:29 +0100

    debug distances.py by passing it with chunks. add einops to requirements.txt

--- code/distances.py
+++ code/distances.py
@@ -21,31 +21,42 @@ def charger_index(chemin_index:str, nombre_phrases:int):
 
 def ordonner_index(distances:np.ndarray, indices:np.ndarray):
     indices_plus_similaires = distances[:,1].argsort()[::-1]
-    indices_ordonnés, distances_ordonnées = (indices[indices_plus_similaires, :], \
-                                             distances[indices_plus_similaires, :])
+    print("Ordonnancement de l'index...")
+    indices_ordonnés, distances_ordonnées = (indices[indices_plus_similaires, :], distances[indices_plus_similaires, :])
     return indices_ordonnés, distances_ordonnées
 
 def faire_table(indices_ordonnés:np.ndarray, distances_ordonnées:np.ndarray,\
-                phrases:list):
-    I0 = indices_ordonnés[:, 0]
-    I1 = indices_ordonnés[:, 1]
-    I2 = indices_ordonnés[:, 2]
-    I3 = indices_ordonnés[:, 3]
-    dictionnaire = {"P1": phrases[I1], "D1": distances_ordonnées[:, 1], \
-                    "P2": phrases[I2], "D2": distances_ordonnées[:, 2], \
-                    "P3": phrases[I3], "D3": distances_ordonnées[:, 3]}
-    df = pd.DataFrame(dictionnaire, index=phrases[I0])
+                phrases:list, taille_bout=5000):
+    phrases_array = np.array(phrases)
     fichier = os.path.join(chemin_resultats, "voisins.csv")
-    df.to_csv(fichier)
+    
+    for début in range(0, len(indices_ordonnés), taille_bout):
+        fin = min(début + taille_bout, len(indices_ordonnés))
+        bout_indices = indices_ordonnés[début:fin]
+        bout_distances = distances_ordonnées[début:fin]
+        
+        I0 = bout_indices[:, 0]
+        I1 = bout_indices[:, 1]
+        I2 = bout_indices[:, 2]
+        I3 = bout_indices[:, 3]
+        
+        dictionnaire = {"P0": phrases_array[I0],
+                        "P1": phrases_array[I1], "D1": bout_distances[:, 1],
+                        "P2": phrases_array[I2], "D2": bout_distances[:, 2],
+                        "P3": phrases_array[I3], "D3": bout_distances[:, 3]}
+        df = pd.DataFrame(dictionnaire, index=I0)
+        
+        df.to_csv(fichier, mode='a', header=(début==0))
 
 def main():
     with open(chemin_phrases, "rb") as t:
         phrases = pickle.load(t)
-    n = len(phrases)
-    d, i = charger_index(chemin_index, n)
+    d, i = charger_index(chemin_index, len(phrases))
     i_ordonnés, d_ordonnées = ordonner_index(d, i)
     faire_table(i_ordonnés, d_ordonnées, phrases)
 
+if __name__ == "__main__":
+    main()
 
 
 
