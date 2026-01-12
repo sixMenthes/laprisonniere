95924364c6264c2aa243bedd1941acedbecd5059
sixmenthes <six.menthes@gmail.com>
2026-01-12 16:28:02 +0100

    add debug prints

--- src/mahalanobis.py
+++ src/mahalanobis.py
@@ -7,21 +7,24 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"CUDA disponible: {torch.cuda.is_available()}")
 
 def transformation_mahalanobis(echantillon):
-    echantillon = Vectoriseur(echantillon, dimensions=32).encoder_par_batch().to(device)
+    echantillon = Vectoriseur(echantillon, dimensions=32).encoder_par_batch().to(device) # 50x32
+    print(f"Forme echantillon = {echantillon.Size()}")
     with torch.no_grad():
         moyenne = echantillon.mean(0)
         echantillon = echantillon - moyenne
-        cov = torch.mm(echantillon.t(), echantillon) / echantillon.size(0)
+        cov = torch.mm(echantillon.t(), echantillon) / echantillon.size(0) 
         L = torch.linalg.cholesky(cov)
-        inv = torch.linalg.inv(L)
+        inv = torch.linalg.inv(L) # 32x32
+    print(f"Forme inverse = {inv.Size()}")
     return inv, moyenne
 
 def mapper_vecteurs(corpus, inverse_covariance, moyenne):
-    corpus = Vectoriseur(corpus, dimensions=32).encoder_par_batch().to(device)
+    corpus = Vectoriseur(corpus, dimensions=32).encoder_par_batch().to(device) # 40980 x 32
+    print(f"Forme corpus = {corpus.Size()}")
     with torch.no_grad():
-        corpus = torch.mm(corpus, inverse_covariance.t())
         corpus = corpus - moyenne 
-        corpus = torch.linalg.norm(corpus, dim=1)
+        corpus = torch.mm(corpus, inverse_covariance.t()) # 40980 x 32
+        corpus = torch.linalg.norm(corpus, dim=1) # 40980
     return corpus.cpu()
 
 def construire_csv(normes, corpus:Corpus):
@@ -37,9 +40,12 @@ def construire_csv(normes, corpus:Corpus):
 
 def main():
     echantillon = Corpus("phrases_son.txt", style="echantillon")
+    print(f"Longueur echantillon: {len(echantillon)}")
     inv, moyenne = transformation_mahalanobis(echantillon.texte)
     corpus = Corpus("corpus_echos/", style="corpus")
+    print(f"Longueur corpus: {len(corpus)}")
     normes = mapper_vecteurs(corpus.texte, inv, moyenne).numpy()
+    print(f"Forme normes = {normes.shape}")
     construire_csv(normes, corpus)
 
 
